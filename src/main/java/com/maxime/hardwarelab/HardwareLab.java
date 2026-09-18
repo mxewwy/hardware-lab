6f0bd3530979455255a022414c7737c2a30316e7
mxewwy <max.kipper.tlt@gmail.com>
2026-09-18 13:01:45 +0300

    Add mod entrypoint

---
+++ src/main/java/com/maxime/hardwarelab/HardwareLab.java
@@ -0,0 +1,15 @@
+package com.maxime.hardwarelab;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class HardwareLab implements ModInitializer {
+    public static final String MOD_ID = "hardware_lab";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Hardware Lab initialized");
+    }
+}
