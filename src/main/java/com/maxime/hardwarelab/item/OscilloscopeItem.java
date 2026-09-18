fd6b4f4b95bc61367eee68ff39a63a0824d35158
mxewwy <max.kipper.tlt@gmail.com>
2026-09-18 18:04:28 +0300

    Improve wire interaction and localize hardware tools

--- src/main/java/com/maxime/hardwarelab/item/OscilloscopeItem.java
+++ src/main/java/com/maxime/hardwarelab/item/OscilloscopeItem.java
@@ -1,9 +1,17 @@
 package com.maxime.hardwarelab.item;
 
+import com.maxime.hardwarelab.HardwareLabLanguage;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 
 public final class OscilloscopeItem extends Item {
     public OscilloscopeItem(Properties properties) {
         super(properties);
+    }
+
+    @Override
+    public Component getName(ItemStack stack) {
+        return HardwareLabLanguage.component("item.oscilloscope");
     }
 }
