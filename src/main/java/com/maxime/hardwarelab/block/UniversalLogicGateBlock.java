9f54e7ca3233d4940ea42b8fe8a0dbc678759355
mxewwy <max.kipper.tlt@gmail.com>
2026-09-18 17:16:43 +0300

    Fix universal logic gate block state registration

--- src/main/java/com/maxime/hardwarelab/block/UniversalLogicGateBlock.java
+++ src/main/java/com/maxime/hardwarelab/block/UniversalLogicGateBlock.java
@@ -8,6 +8,7 @@ import net.minecraft.core.Direction;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
@@ -40,8 +41,13 @@ public class UniversalLogicGateBlock extends HorizontalDirectionalBlock {
 
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        super.createBlockStateDefinition(builder);
-        builder.add(GATE_TYPE);
+        builder.add(FACING, GATE_TYPE);
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return defaultBlockState()
+                .setValue(FACING, context.getHorizontalDirection().getOpposite());
     }
 
     @Override
