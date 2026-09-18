7e4ce643284c41d837396846e17b5b268e08d80c
mxewwy <max.kipper.tlt@gmail.com>
2026-09-18 18:05:31 +0300

    Add custom hardware component collision

--- src/main/java/com/maxime/hardwarelab/block/BusSplitterBlock.java
+++ src/main/java/com/maxime/hardwarelab/block/BusSplitterBlock.java
@@ -171,4 +171,22 @@ public class BusSplitterBlock extends BaseEntityBlock {
         BlockEntity entity = level.getBlockEntity(pos);
         return entity instanceof BusSplitterBlockEntity splitter ? splitter : null;
     }
+    @Override
+    protected net.minecraft.world.phys.shapes.VoxelShape getShape(
+            net.minecraft.world.level.block.state.BlockState state,
+            net.minecraft.world.level.BlockGetter level,
+            net.minecraft.core.BlockPos pos,
+            net.minecraft.world.phys.shapes.CollisionContext context) {
+        return HardwareShapes.COMPONENT;
+    }
+
+    @Override
+    protected net.minecraft.world.phys.shapes.VoxelShape getCollisionShape(
+            net.minecraft.world.level.block.state.BlockState state,
+            net.minecraft.world.level.BlockGetter level,
+            net.minecraft.core.BlockPos pos,
+            net.minecraft.world.phys.shapes.CollisionContext context) {
+        return HardwareShapes.COMPONENT;
+    }
+
 }
