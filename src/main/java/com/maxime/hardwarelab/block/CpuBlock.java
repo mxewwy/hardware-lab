89ea937326c3358cd777075a36552f338aae750d
mxewwy <max.kipper.tlt@gmail.com>
2026-09-18 13:46:46 +0300

    Fix compute stack code

--- src/main/java/com/maxime/hardwarelab/block/CpuBlock.java
+++ src/main/java/com/maxime/hardwarelab/block/CpuBlock.java
@@ -14,12 +14,11 @@ import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
-import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.StateDefinition;
 import net.minecraft.world.phys.BlockHitResult;
 
 public final class CpuBlock extends BaseEntityBlock implements BusOutputBlock {
