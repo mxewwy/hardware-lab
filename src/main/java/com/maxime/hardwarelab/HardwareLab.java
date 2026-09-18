ede59f0bbabf6df69e356265a373d0e4677ce00f
mxewwy <max.kipper.tlt@gmail.com>
2026-09-18 13:05:50 +0300

    Initialize registered Hardware Lab content

--- src/main/java/com/maxime/hardwarelab/HardwareLab.java
+++ src/main/java/com/maxime/hardwarelab/HardwareLab.java
@@ -1,5 +1,6 @@
 package com.maxime.hardwarelab;
 
+import com.maxime.hardwarelab.block.ModBlocks;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,6 +11,7 @@ public final class HardwareLab implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        ModBlocks.initialize();
         LOGGER.info("Hardware Lab initialized");
     }
 }
