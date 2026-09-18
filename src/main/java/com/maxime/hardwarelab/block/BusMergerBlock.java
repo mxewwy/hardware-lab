732e9ceabc6a7a8d1ec150bba1cb918868b7040d
mxewwy <max.kipper.tlt@gmail.com>
2026-09-18 18:20:24 +0300

    Localize Hardware Lab component messages

--- src/main/java/com/maxime/hardwarelab/block/BusMergerBlock.java
+++ src/main/java/com/maxime/hardwarelab/block/BusMergerBlock.java
@@ -1,5 +1,6 @@
 package com.maxime.hardwarelab.block;
 
+import com.maxime.hardwarelab.HardwareLabLanguage;
 import com.maxime.hardwarelab.block.entity.BusMergerBlockEntity;
 import com.maxime.hardwarelab.block.entity.ModBlockEntities;
 import com.maxime.hardwarelab.logic.BusSignal;
@@ -75,7 +76,7 @@ public final class BusMergerBlock extends BaseEntityBlock implements BusOutputBl
         }
 
         player.sendOverlayMessage(Component.literal(
-                "Bus Merger | WIDTH=" + entity.width().bits()
+                HardwareLabLanguage.blockName("bus_merger") + " | WIDTH=" + entity.width().bits()
                         + " | BANK=" + entity.bank()
                         + " | BITS=" + entity.firstBit() + "-" + (entity.firstBit() + 3)
         ));
