30bc4eef96cd524a9a789d9a31470b559f88d864
mxewwy <max.kipper.tlt@gmail.com>
2026-09-18 18:20:46 +0300

    Localize Hardware Lab component messages

--- src/main/java/com/maxime/hardwarelab/block/FpgaBlock.java
+++ src/main/java/com/maxime/hardwarelab/block/FpgaBlock.java
@@ -1,5 +1,6 @@
 package com.maxime.hardwarelab.block;
 
+import com.maxime.hardwarelab.HardwareLabLanguage;
 import com.maxime.hardwarelab.block.entity.FpgaBlockEntity;
 import com.maxime.hardwarelab.block.entity.ModBlockEntities;
 import com.maxime.hardwarelab.logic.BusSignal;
@@ -107,7 +108,7 @@ public final class FpgaBlock extends BaseEntityBlock implements BusOutputBlock {
         }
 
         player.sendOverlayMessage(Component.literal(
-                "FPGA 4-LUT | MODE=" + entity.mode().displayName()
+                HardwareLabLanguage.blockName("fpga") + " | MODE=" + entity.mode().displayName()
                         + " | " + (entity.registered() ? "REGISTERED" : "COMBINATIONAL")
                         + " | LUT=0x" + String.format("%04X", FpgaLut.truthTable(entity.mode()))
         ));
